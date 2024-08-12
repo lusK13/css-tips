12e407ede8a2918c7b22ad5d53554dabee782af5
lusK13 <57751289+lusK13@users.noreply.github.com>
2024-08-12 16:04:41 +0200

    Update convertData.js

--- convertData.js
+++ convertData.js
@@ -1,3 +1,53 @@
+function convertDatas2(
+  datas,
+  requiredDatas = [],
+  optionalDatas = [],
+  convertProperty
+) {
+  const result = {};
+
+  for (const key of requiredDatas) {
+    if (datas[key] !== undefined) {
+      result[key] = datas[key];
+    }
+  }
+
+  for (const key of optionalDatas) {
+    if (datas[key] !== undefined) {
+      result[key] = datas[key];
+    }
+  }
+
+  if (convertProperty) {
+    for (const key in convertProperty) {
+      if (!(requiredDatas.includes(key) || optionalDatas.includes(key))) {
+        throw new Error(`Missing in required or optional data: ${key}`);
+      }
+      const convertFunction = convertProperty[key];
+      if (typeof convertFunction === "function") {
+        result[key] = convertFunction(datas[key], datas);
+      } else {
+        result[key] = convertFunction;
+      }
+      if (result[key] === undefined) {
+        if (requiredDatas.includes(key)) {
+          throw new Error(`Missing required data: ${key}`);
+        }
+        delete result[key];
+      }
+    }
+  }
+
+  const missingDatas = requiredDatas.filter(
+    (key) => !Object.keys(result).includes(key)
+  );
+
+  if (missingDatas.length > 0) {
+    throw new Error(`Missing required data: ${missingDatas.join(", ")}`);
+  }
+  return result;
+}
+
 function convertDatas(
   datas,
   requiredDatas = [],
@@ -39,5 +89,20 @@ function convertDatas(
   if (missingDatas.length > 0) {
     throw new Error(`Missing required data: ${missingDatas.join(", ")}`);
   }
+  return result;
+}
+function remapKeysDatas(datas, oldKeys, newKeys, deleteOtherKeys = false) {
+  const result = {};
+  for (const key in datas) {
+    const index = oldKeys.indexOf(key);
+    if (index !== -1) {
+      result[newKeys[index]] = datas[key];
+    } else {
+      if (!deleteOtherKeys) {
+        result[key] = datas[key];
+      }
+    }
+  }
+
   return result;
 }
